f9579888284b3e1b1110db7239f39c7928a58214
christopher shih <chrshih875@gmail.com>
2024-07-16 14:22:18 -0700

    debug

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://52.53.233.152:8000",
-    "http://todo-list-website.s3-website-us-west-1.amazonaws.com/",
+    "http://todo-list-website.s3-website-us-west-1.amazonaws.com",
 ]
 
 
