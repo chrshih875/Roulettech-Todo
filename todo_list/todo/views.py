225db29e57bf422370962cb7615419a96dd69f97
christopher shih <chrshih875@gmail.com>
2024-07-15 18:58:33 -0700

    adding task status

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+    @action(detail=True, methods=['post'])
+    def mark_as_completed(self, request, pk=None):
+        todo = self.get_object()
+        todo.completed = True
+        todo.save()
+        serializer = self.get_serializer(todo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        completed = self.request.query_params.get('completed', None)
+        if completed is not None:
+            queryset = queryset.filter(completed=completed.lower() == 'true')
+        return queryset
