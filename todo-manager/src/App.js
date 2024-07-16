c44d4ea2ea89b5e07d6e8d7be4e10566ec40c6f5
christopher shih <chrshih875@gmail.com>
2024-07-16 13:21:15 -0700

    clean up for build

--- todo-manager/src/App.js
+++ todo-manager/src/App.js
@@ -2,10 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import TodoList from "./components/AllTasks";
 import CreateTask from "./components/CreateTask";
-import CompletedTasks from "./components/CompletedTask";
-import IncompleteTasks from "./components/IncompleteTask";
 import TaskManager from "./components/parent";
 
 function App() {
@@ -13,10 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          {/* <Route path="/" element={<TodoList />} /> */}
           <Route path="/create" element={<CreateTask />} />
-          <Route path="/comp" element={<CompletedTasks />} />
-          <Route path="/incomp" element={<IncompleteTasks />} />
           <Route path="/" element={<TaskManager />} />
         </Routes>
       </div>
